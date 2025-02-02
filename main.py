c1a27e14ecd1d1b2db5bc9895b50eb43c0a4d42a
shayanshater <shayanshaterpoori@outlook.com>
2025-02-02 19:24:52 +0000

    Made a dataframe with sales, data and region of pink morsel

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+df0 = pd.read_csv('data/daily_sales_data_0.csv')
+df1 = pd.read_csv('data/daily_sales_data_1.csv')
+df2 = pd.read_csv('data/daily_sales_data_2.csv')
+df = pd.concat([df0,df1,df2], ignore_index=True)
+
+
+
+df = df[df['product'] == 'pink morsel']
+
+
+df['price'] = df['price'].str.replace('$','',regex=False).astype(float)
+df['quantity'] = df['quantity'].astype(int)
+df['sales'] = df['price'] * df['quantity']
+
+
+df = df[['sales','date','region']]
+
+print(df)
+
