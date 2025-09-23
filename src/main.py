fb543e77102bd11378df8c68ea9f707bc1f747a7
shayan <shayanshaterpoori@outlook.com>
2025-09-23 18:10:44 +0100

    create a radio button that filters by regoin

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import plotly.express as px
 import pandas as pd
 import os
 
@@ -18,7 +20,67 @@ def make_dataframe(directory):
     
     #create final df
     df = df[["date", "sales", "region"]]
+    
+    # sorting by date
+    df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
     return df
+
+
+def dash_app(df):
+    # Run this app with `python app.py` and
+    # visit http://127.0.0.1:8050/ in your web browser.
+
+
+    colors = {
+    'line': "#6F00FF",
+    'text': "#3B0270",
+    "background": "#FFF1F1"
+    
+    }
+
+    app = Dash()
+    app.layout = html.Div([
+            html.Div([
+                html.Div(children='Sales Revenue Against Time', style={
+                'textAlign': 'center',
+                'color': colors['text']}), 
+
+            html.Div([
+                dcc.RadioItems(
+                    df.region.unique(),
+                    'north',
+                    id='region-name',
+                    inline=True
+                )
+            ], style={'width': '48%', 'display': 'inline-block'}),
+
+        ]),
+
+               dcc.Graph(id='sales-graph')
+    ])
+    
+    @callback(
+        Output("sales-graph", "figure"), 
+        Input("region-name", "value")
+    )
+    def update_graph(region):
+        df_region = df[df["region"] == region]
+        
+        fig = px.scatter(x = df_region["date"], y = df_region["sales"])
+        
+        fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+        )
+        
+        return fig
+    return app
+
+if __name__ == '__main__':
+    df = make_dataframe("data/")
+    app = dash_app(df)
+    app.run(debug=True)
 
 
 
